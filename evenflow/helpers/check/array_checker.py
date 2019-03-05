ed9304530995a07f6243cdc9b11603cc35f3db09
underscorefan <noelijonombres@gmail.com>
2019-03-05 17:43:08 +0100

    fixed typing

--- evenflow/helpers/check/array_checker.py
+++ evenflow/helpers/check/array_checker.py
@@ -1,7 +1,7 @@
-from typing import List, Callable
+from typing import List, Callable, Any
 
 
-def check_list(lst: List, condition: Callable[[any, ], bool], apply: Callable[[any,], any]) -> List:
+def check_list(lst: List, condition: Callable[[Any, ], bool], apply: Callable[[Any, ], Any]) -> List:
     return [apply(element) for element in lst if condition(element)]
 
 
