168ad73a22aaf5abd1cdb3d9894d660846e22dd6
underscorefan <noelijonombres@gmail.com>
2019-03-13 17:56:43 +0100

    created producers package

---
+++ evenflow/producers/__init__.py
@@ -0,0 +1,1 @@
+from .links import produce_links, LinkProducerSettings
--- evenflow/readers/man.py
+++ evenflow/readers/man.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from typing import List, ItemsView, Tuple, Dict, Optional
+from typing import List
 from aiohttp import ClientSession
 # from collections import defaultdict
 
@@ -11,72 +11,7 @@ from evenflow.helpers.unreliableset import UnreliableSet
 from evenflow.messages import LinkContainer
 
 from .htmlreader import FeedReaderHTML
-from evenflow.readers import FeedReader, State, FeedResult
-
-
-class Sender:
-    def __init__(self):
-        self.__links: Dict[str, Tuple[str, bool]] = dict()
-        self.__backup: Dict[str, Dict] = dict()
-        self.__readers: List[FeedReader] = []
-
-    def add_reader(self, reader: Optional[FeedReader]):
-        if reader:
-            self.__readers.append(reader)
-
-    def get_readers(self) -> List[FeedReader]:
-        return self.__readers
-
-    def add_links(self, links: Dict[str, Tuple[str, bool]]):
-        self.__links = {**self.__links, **links}
-
-    def add_to_backup(self, s: State):
-        key, value = s.unpack()
-        self.__backup[key] = value
-
-    def get_links(self) -> Dict[str, Tuple[str, bool]]:
-        return self.__links
-
-    def get_backup(self) -> Dict[str, Dict]:
-        return self.__backup
-
-
-class IterationManager:
-    def __init__(self, initial_readers: List[FeedReader]):
-        self.__readers = initial_readers
-
-    def has_readers(self) -> bool:
-        return len(self.__readers) > 0
-
-    def set_readers(self, readers: List[FeedReader]):
-        self.__readers = readers
-
-    def get_readers(self) -> List[FeedReader]:
-        return self.__readers
-
-
-class LinkProducerSettings:
-    def __init__(self, tracker: HostTracker, unrel: UnreliableSet, send_channel: asyncio.Queue, session: ClientSession):
-        self.tracker = tracker
-        self.unrel = unrel
-        self.send_channel = send_channel
-        self.session = session
-
-
-async def produce_links(settings: LinkProducerSettings, to_read: List[FeedReader]):
-    iteration_manager = IterationManager(to_read)
-    while iteration_manager.has_readers():
-        coroutines = [source.fetch_links(settings.session) for source in iteration_manager.get_readers()]
-        sender = Sender()
-
-        for res in await asyncio.gather(*coroutines):
-            feed_result: FeedResult = res
-            sender.add_reader(feed_result.next_reader)
-            sender.add_links({k: v for k, v in feed_result.links.items() if settings.unrel.contains(k)})
-            sender.add_to_backup(feed_result.current_reader_state)
-
-        await settings.send_channel.put(LinkContainer(links=sender.get_links(), backup=sender.get_backup()))
-        iteration_manager.set_readers(sender.get_readers())
+from evenflow.readers import FeedReader, FeedResult
 
 
 class SourceManager:
